f6de69f2aed81ec435fbce27e854f1075fcb2f79
Onur Kilicer <otuzyedidebir@gmail.com>
2022-12-30 13:36:01 +0300

    Program's working principles and future features added (+some minor changes)

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+/*func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
@@ -28,14 +28,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "hello!")
-}
+}*/
 
 func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	//http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port: 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
